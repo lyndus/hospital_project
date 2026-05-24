3f9757a4218ece1f1a68df81a71364fc0567c52d
rawa-kr <rr.khames@esi-sba.dz>
2026-05-24 14:10:10 +0100

    fix allowed host again

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -5,8 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY') 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['web-production-aba75.up.railway.app'] 
+ALLOWED_HOSTS = ['web-production-aba75.up.railway.app'] 
 #ALLOWED_HOSTS = ['pediatric-hms.onrender.com']
 
 INSTALLED_APPS = [
